8e341c2e79abd6be56aded34f73751cb56142948
jaidetura55 <44111666+jaidetura55@users.noreply.github.com>
2024-01-30 10:50:39 +0800

    Update

---
+++ speedtest-cli.sh
@@ -0,0 +1,36 @@
+echo -e """\e[1;38;5;214m
+  _______________
+ <\e[1;36m speedtest-cli\e[1;38;5;214m >\e[1;38;5;214m
+  --------------- \e[1;32m
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )/|
+                ||----w |
+                ||     || \e[0m
+
+
+                """
+
+
+echo "Auto installation script of SpeedTest Cli"
+echo "             modded by kimmy619"
+sleep 4
+
+clear
+cd $HOME
+echo "Download Starting....."
+sleep 2
+curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest-cli
+cp speedtest-cli  speedtest
+chmod +x speeedtest
+mv speedtest $PREFIX/bin
+rm -rf speedtest-cli
+clear
+
+echo "SpeedTest installed"
+sleep 2
+echo ""
+echo " type speedtest to begin or ./speedtest"
+sleep 1
+exit 1
